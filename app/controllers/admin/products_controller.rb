6899de52dd3fb6871b0a32a184745fe94a021040
Kapil Goyal <kapilgoyal6@gmail.com>
2020-11-10 01:34:55 +0530

    initial

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,65 @@
+class Admin::ProductsController < Admin::ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy, :approve]
+  def index
+    @products = Product.all.order('id DESC')
+  end
+
+  def show
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to [:admin, @product], notice: 'Product was successfully update'
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user_id = current_user.id
+    if @product.save
+      redirect_to [:admin, @product], notice: 'Product was successfully created'
+    else
+      render :new
+    end
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  def destroy
+    @product = Product.find_by_id(params[:id])
+    @product.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  def approve
+    @product.update(status: true)
+
+    new_product = ShopifyAPI::Product.new
+    new_product.title = @product.name
+    new_product.description = @product.description
+    new_product.stock = @product.available_quantity
+    new_product.price = @product.price
+    # new_product.product_type = "Snowboard"
+    # new_product.vendor = "Burton"
+    new_product.save
+
+    redirect_to admin_products_path, notice: 'Product is approved'
+  end
+
+  private
+  def set_product
+    @product = Product.find_by_id(params[:id])
+  end
+  def product_params
+    params.require(:product).permit(:user_id, :name, :description, :image, :available_quantity, :price, :size, :status, :online)
+  end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,24 @@
+class Product < ApplicationRecord
+
+  has_many :product_collections
+  has_many :collections, through: :product_collections
+  mount_uploader :image, AvatarUploader
+  belongs_to :user
+
+  def get_image_url
+    return self.image && self.image.url ? self.image.try(:url) : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxzT3N7-TEsb1AD_eU8B2TtTLryBLItF7cKw&usqp=CAU'
+  end
+
+  def status_name
+    self.status ? "approved" : "pending"
+  end
+
+  def is_pending?
+    !self.status
+  end
+
+  def is_approved?
+    self.status
+  end
+    
+end
