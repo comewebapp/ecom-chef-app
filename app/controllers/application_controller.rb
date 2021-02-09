bf1c7556337f3f6a7f5f82b92ba3ce01cf38b9bb
Kapil Goyal <kapilgoyal6@gmail.com>
2021-02-09 18:05:26 +0530

    vendor script and update profile fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
   	added_attrs = %i[vendor name lastname email password password_confirmation contact address postalcode city bankaccount bankname bankowner carnet]
-  	devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
 end
