2f768b642e85e89efa22608ee167491098ccae89
Kapil Goyal <kapilgoyal6@gmail.com>
2021-02-18 22:17:14 +0530

    Forgot Password and email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+  config.action_mailer.default_url_options = { :host => ENV['HOST_URL'] }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default :charset => "utf-8"
