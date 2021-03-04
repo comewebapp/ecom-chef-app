a7d80f0e040ff466b8900b738471a8d23f87516e
Kapil Goyal <kapilgoyal6@gmail.com>
2021-03-04 23:54:16 +0530

    inventory

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,12 +9,11 @@ class Admin::ProductsController < Admin::ApplicationController
 
   def update
     if @product.update(product_params)
-
       @product.product_collections.destroy_all
       params[:product][:collection].reject(&:empty?).each do |collection_id|
         ProductCollection.create(product_id: @product.id, collection_id: collection_id)
       end
-        
+      update_on_shopify
       redirect_to [:admin, @product], notice: 'Product was successfully update'
     else
       render :edit
@@ -63,6 +62,13 @@ class Admin::ProductsController < Admin::ApplicationController
 
       @product.update(shopify_product_id: new_product.id)
 
+      # inventory item
+      inventory_item = ShopifyAPI::InventoryItem.find new_product.variants[0].inventory_item_id rescue nil
+      if inventory_item
+        inventory_item.tracked = true
+        inventory_item.save!
+      end
+
       if @product.image_base64.present?
         i = ShopifyAPI::Image.new
         i.attach_image(Base64.decode64(@product.image_base64.gsub("data:image/png;base64,", ""))) # <-- attach_image is a method, not an attribute
@@ -110,10 +116,82 @@ class Admin::ProductsController < Admin::ApplicationController
   end
 
   private
+
   def set_product
     @product = Product.find_by_id(params[:id])
   end
+
   def product_params
     params.require(:product).permit(:user_id, :name, :description, :image, :available_quantity, :price, :size, :status, :online)
   end
+
+  def update_on_shopify
+    new_product = ShopifyAPI::Product.find @product.shopify_product_id rescue nil
+    new_product = ShopifyAPI::Product.new if new_product.nil?
+    new_product.title = @product.name
+    new_product.body_html = @product.description
+    new_product.product_type = "Plato"
+    new_product.vendor = @product.user.vendor
+    new_product.save
+    new_product.variants[0].price = @product.price
+    new_product.variants[0].sku = @product.available_quantity
+    # new_product.variants[0].inventory_quantity = @product.available_quantity
+    new_product.variants[0].weight = @product.weight
+
+    if new_product.save!
+
+      @product.update(shopify_product_id: new_product.id)
+
+      # inventory item
+      inventory_item = ShopifyAPI::InventoryItem.find new_product.variants[0].inventory_item_id rescue nil
+      if inventory_item
+        inventory_item.tracked = true
+        inventory_item.save
+      end
+
+      if @product.image_base64.present?
+        i = ShopifyAPI::Image.new
+        i.attach_image(Base64.decode64(@product.image_base64.gsub("data:image/png;base64,", ""))) # <-- attach_image is a method, not an attribute
+        new_product.images << i
+
+        new_product.save
+      end
+
+      # ingredients
+      if @product.ingredients.present?
+        new_product.add_metafield(ShopifyAPI::Metafield.new({
+          namespace: 'come',
+          key: 'ingredientes',
+          value: @product.ingredients,
+          value_type: 'string'
+        }))
+      end
+
+      # disponibleDias
+      new_product.add_metafield(ShopifyAPI::Metafield.new({
+        namespace: 'come',
+        key: 'disponibleDias',
+        value: @product.delivery_days.gsub(", ", "|"),
+        value_type: 'string'
+      }))
+
+      # disponibleFranjas
+      new_product.add_metafield(ShopifyAPI::Metafield.new({
+        namespace: 'come',
+        key: 'disponibleFranjas',
+        value: @product.delivery_hours.gsub(", ", "|"),
+        value_type: 'string'
+      }))
+
+      @product.product_collections.each do |product_collection|
+        collection = ShopifyAPI::CustomCollection.find(product_collection.collection_id)
+        collection.add_product new_product
+      end
+      # @product.update(status: true)
+      # redirect_to admin_products_path, notice: 'Product is approved'
+    # else
+      # redirect_to admin_products_path, alert: 'Product is not approved'
+    end
+  end
+
 end
